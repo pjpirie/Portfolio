e10ae3c2fb62976b77c74a4565f5fcea07ac239a
pjpirie <pauljohnpirie@yahoo.co.uk>
2019-01-06 23:02:46 +0000

    About Section And Bluring Elements When HamBtn is clicked

--- web/script.js
+++ web/script.js
@@ -1,21 +1,40 @@
 const ham = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
-const bg = document.querySelector('#hero');
+const blur = document.querySelector('[blurable]');
 
 ham.classList.remove("active");
-bg.classList.remove("blur");
+console.log(document.stylesheet);
 function hamClicked() {
-	console.log("click");
- if(ham.classList.contains("active")){
- 	console.log("Unclicked");
- 	ham.classList.remove("active");
- 	menu.classList.add("hidden");
- 	bg.classList.remove("blur");
- }else{
- 	console.log("Clicked");
- 	console.log(ham.classList);
- 	ham.classList.add("active"); 
- 	bg.classList.add("blur");
- 	menu.classList.remove("hidden");
- };
+
+	toggleClass(document.querySelector('.hamburger'), "active");
+	toggleClass(document.querySelector('.menu'), "hidden");
+	toggleAllClass(document.querySelectorAll('[blurable]'), "blur");
+	// toggleClassAllSelector(document.querySelector('.blurable'), "blur");
+
+ // if(ham.classList.contains("active")){
+ // 	ham.classList.remove("active");
+ // 	menu.classList.add("hidden");
+ // 	toggleClassAllSelector();
+ // }else{
+ // 	ham.classList.add("active"); 
+ // 	blur.classList.add("blur");
+ // 	menu.classList.remove("hidden");
+ // };
+}
+
+
+function toggleAllClass(target, classToToggle){
+ console.log(target.length);
+	 for(let i = 0; i<target.length;i++){
+	 	toggleClass(target[i], classToToggle);
+	 	console.log('Modified : ' + target[i] + ' Toggled : ' + classToToggle);
+	 }	
+}
+
+function toggleClass(target, classToToggle){
+	if(target.classList.contains(classToToggle)){
+ 		target.classList.remove(classToToggle);
+	 }else{
+	 	target.classList.add(classToToggle); 
+	 };
 }
